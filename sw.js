3244375b4c4f6e9c4b57d0b29cc191d2e74d1628
sercan25061-hub <sercan.25061@gmail.com>
2026-03-20 03:42:11 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'dsm-cache-v1';
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
